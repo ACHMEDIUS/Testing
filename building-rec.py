ae72e2990be2afe6c1322ff3df170a6a797f8037
ACHMEDIUS <98095760+ACHMEDIUS@users.noreply.github.com>
2023-12-18 11:02:40 +0000

    Succesfull in getting seperate yellow regions

--- building-rec.py
+++ building-rec.py
@@ -1,29 +1,45 @@
 import cv2
 import numpy as np
 
-def highlight_specific_color(image_path, target_color, output_path):
+def highlight_and_fill_yellow_regions(image_path, output_path):
     # Read the image
     image = cv2.imread(image_path)
-
-    # Convert the image from BGR to RGB (OpenCV uses BGR by default)
+    original_shape = image.shape  # Save the original shape for later
+    
+    # Convert to the RGB color space
     image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    
+    # Define a color range for yellow, allowing for some variation
+    lower_yellow = np.array([150, 150, 0], dtype=np.uint8)
+    upper_yellow = np.array([255, 255, 50], dtype=np.uint8)
+    
+    # Create a mask for yellow color
+    mask_yellow = cv2.inRange(image_rgb, lower_yellow, upper_yellow)
+    
+    # Use morphological operations to close gaps - dilation followed by erosion
+    kernel = np.ones((3,3), np.uint8)
+    mask_closed = cv2.morphologyEx(mask_yellow, cv2.MORPH_CLOSE, kernel)
+    
+    # Find contours in the mask and fill them
+    contours, _ = cv2.findContours(mask_closed, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    mask_filled = cv2.drawContours(mask_closed.copy(), contours, -1, (255), thickness=cv2.FILLED)
+    
+    # Bitwise-AND mask and original image
+    result = cv2.bitwise_and(image_rgb, image_rgb, mask=mask_filled)
+    
+    # Convert the result to BGR before saving
+    result_bgr = cv2.cvtColor(result, cv2.COLOR_RGB2BGR)
+    
+    # Ensure the resolution matches the original image
+    if result_bgr.shape != original_shape:
+        result_brg = cv2.resize(result_bgr, (original_shape[1], original_shape[0]), interpolation=cv2.INTER_AREA)
+    
+    # Save the result
+    cv2.imwrite(output_path, result_bgr)
 
-    # Define the lower and upper bounds for the target color (in RGB)
-    lower_bound = np.array(target_color, dtype=np.uint8)
-    upper_bound = np.array(target_color, dtype=np.uint8)
+# Usage example
+image_path = 'destination_map.jpeg'  # Replace with the path to your input image
+output_path = 'output.png'  # Replace with the path to save the output image
 
-    # Create a binary mask for pixels within the specified color range
-    mask = cv2.inRange(image_rgb, lower_bound, upper_bound)
-
-    # Bitwise AND operation to extract the region of interest
-    result = cv2.bitwise_and(image, image, mask=mask)
-
-    # Save or display the result
-    cv2.imwrite(output_path, cv2.cvtColor(result, cv2.COLOR_BGR2RGB))
-
-# Example usage
-image_path = 'destination_map.jpeg'
-target_color = [255, 255, 1]  # RGB values for #ff543f
-output_path = 'output.jpg'
-
-highlight_specific_color(image_path, target_color, output_path)
+# Run the function
+highlight_and_fill_yellow_regions(image_path, output_path)
---
+++ test.py
@@ -0,0 +1,34 @@
+import cv2
+import numpy as np
+import os
+
+def create_directory_if_not_exists(directory):
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+def extract_and_save_yellow_regions(image_path, output_folder):
+    create_directory_if_not_exists(output_folder)
+    image = cv2.imread(image_path)
+    image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    lower_yellow = np.array([150, 150, 0], dtype=np.uint8)
+    upper_yellow = np.array([255, 255, 50], dtype=np.uint8)
+    mask_yellow = cv2.inRange(image_rgb, lower_yellow, upper_yellow)
+    kernel = np.ones((3,3), np.uint8)
+    mask_closed = cv2.morphologyEx(mask_yellow, cv2.MORPH_CLOSE, kernel)
+    contours, _ = cv2.findContours(mask_closed, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    region_file_paths = []
+
+    for i, contour in enumerate(contours):
+        mask = np.zeros_like(mask_yellow)
+        cv2.drawContours(mask, [contour], -1, color=255, thickness=cv2.FILLED)
+        region = cv2.bitwise_and(image_rgb, image_rgb, mask=mask)
+        region_filename = f"{output_folder}/region_{i}.png"
+        cv2.imwrite(region_filename, cv2.cvtColor(region, cv2.COLOR_RGB2BGR))
+        region_file_paths.append(region_filename)
+    
+    return region_file_paths
+
+# Example usage
+image_path = 'destination_map.jpeg'
+output_folder = './regions_output'
+region_file_paths = extract_and_save_yellow_regions(image_path, output_folder)
